e00d4a9386f5d3f252e68c193d484a87dd0cd128
knightkk22 <43912807+knightkk22@users.noreply.github.com>
2019-10-31 16:19:58 -0500

    Create Chapter9.cs

---
+++ Chapter9.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_9
+{
+    public partial class Birthday : Form
+    {
+        new findDay();
+        public Birthday()
+        {
+            InitializeComponent();
+        }
+
+        private void NumericMonth_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
